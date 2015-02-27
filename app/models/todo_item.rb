359f179e0d34f39795f8363f211bd76e348ec906
Justin LeFurjah <lefurjah@gmail.com>
2015-02-27 13:21:30 +0100

    clean up todo item code

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,5 +3,10 @@ class TodoItem < ActiveRecord::Base
 
   validates :content, presence: true
   validates :content, length: { minimum: 2 }
+
+  def completed?
+  	!completed_at.blank?
+  end
+  
 end
 
